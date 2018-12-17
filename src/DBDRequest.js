7b17920936cc79c96d0eabc977c545db73cf477d
Hazmi35 <hazminoname@gmail.com>
2018-12-17 13:06:03 +0700

    move from SUPERAGENT to DBDREQUEST

---
+++ src/DBDRequest.js
@@ -0,0 +1,120 @@
+const https = require('https');
+const version = require('../package.json').version;
+//console.log(version);
+
+class DBDRequest {
+    /**
+     * Creates DBDRequest Client
+     * @param {String} baseAPIUrl BaseAPIURL to use
+     * @private
+     */
+    constructor(baseAPIUrl) {
+        this.baseAPIUrl = baseAPIUrl;
+        this.version = '1.3.0-dbdrequest';
+
+        /**
+         * Creates a GET request
+         * @param {String} endpoint API Endpoint to use.
+         * @private
+         * @returns {Promise<Object>} A Promise containing response
+         */
+        this.get = (endpoint) => {
+            return new Promise((resolve, reject) => {
+                const response = {
+                    method: 'GET',
+                    version: '1.3.0-dbdrequest',
+                    raw: '',
+                    body: null,
+                    status: null,
+                    headers: null
+                };
+                const options = {
+                    hostname: baseAPIUrl,
+                    path: `/api/${endpoint}`,
+                    method: 'GET',
+                    headers: {}
+                };
+                options.headers['user-agent'] = `dbdapi.js/${version}`;
+                options.headers['content-type'] = 'application/json';
+                const request = https.request(options, res => {
+                    response.status = res.statusCode;
+                    response.headers = res.headers;
+                    response.ok = res.statusCode >= 200 && res.statusCode < 300;
+                    res.on('data', chunk => {
+                        response.raw += chunk;
+                    });
+                    res.on('end', () => {
+                        response.body = res.headers['content-type'].includes('application/json') ? JSON.parse(response.raw) : response.raw;
+                        if (response.ok) {
+                            resolve(response);
+                        } else {
+                            const err = new Error(`[DBDAPI] ${res.statusCode} ${res.statusMessage}`);
+                            Object.assign(err, response);
+                            reject(err);
+                        }
+                    });
+                });
+
+                request.on('error', err => {
+                    reject(err);
+                });
+
+                request.end();
+            });
+        };
+
+        /**
+         * Creates a POST request
+         * @param {String} endpoint API Endpoint to use.
+         * @param {String} data Data to send
+         * @private
+         * @returns {Promise<Object>} A Promise containing response
+         */
+        this.post = (endpoint, data) => {
+            return new Promise((resolve, reject) => {
+                const response = {
+                    method: 'POST',
+                    version: '1.3.0-dbdrequest',
+                    raw: '',
+                    body: null,
+                    status: null,
+                    headers: null
+                };
+                const options = {
+                    hostname: baseAPIUrl,
+                    path: `/api/${endpoint}`,
+                    method: 'POST',
+                    headers: {}
+                };
+                options.headers['user-agent'] = `dbdapi.js/${version}`;
+                options.headers['content-type'] = 'application/json';
+                const request = https.request(options, res => {
+                    response.status = res.statusCode;
+                    response.headers = res.headers;
+                    response.ok = res.statusCode >= 200 && res.statusCode < 300;
+                    res.on('data', chunk => {
+                        response.raw += chunk;
+                    });
+                    res.on('end', () => {
+                        response.body = res.headers['content-type'].includes('application/json') ? JSON.parse(response.raw) : response.raw;
+                        if (response.ok) {
+                            resolve(response);
+                        } else {
+                            const err = new Error(`[DBDAPI] ${res.statusCode} ${res.statusMessage}`);
+                            Object.assign(err, response);
+                            reject(err);
+                        }
+                    });
+                });
+                request.on('error', err => {
+                    reject(err);
+                });
+
+                request.write(JSON.stringify(data));
+                request.end();
+            });
+        };
+    }
+}
+
+module.exports = DBDRequest;
